b484f966ebee3b84bed777c2bbbccf76e9ee68f5
janssenkoub <janssenkoubikani@gmail.com>
2020-11-22 22:21:10 +0100

    ajout vue à propos

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Mini projet Android-Kotlin [Newsletter](https://github.com/eamosse/android-mbds-2020/blob/main/mini_projet.md).
 
 # Membres du groupe
-LOUBOTA Koubikani <br>
+KOUBIKANI LOUBOTA Janssen <br>
 RANDRIANANDRAINA Manotiana <br>
 PAJANY Allan
 
---
+++ app/src/main/java/com/mbds/newsletter/model/aproposdenous.kt
@@ -0,0 +1,12 @@
+package com.mbds.newsletter.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mbds.newsletter.R
+
+class aproposdenous : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aproposdenous)
+    }
+}
